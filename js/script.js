d4388ad41943f68bc33551be851c2ad7515d8d29
pateldhruvil2008 <dhruvil2008patel@gmail.com>
2026-06-05 11:01:04 +0530

    Auto update

--- js/script.js
+++ js/script.js
@@ -319,3 +319,44 @@ fetch("https://ipapi.co/json/")
   }
 
 });
+// =========================
+// DYNAMIC TIME THEME
+// =========================
+
+const hour =
+new Date().getHours();
+
+const body =
+document.body;
+
+if(hour >= 6 && hour < 12){
+
+  body.classList.add(
+    "morning"
+  );
+
+}
+
+else if(hour >= 12 && hour < 18){
+
+  body.classList.add(
+    "day"
+  );
+
+}
+
+else if(hour >= 18 && hour < 21){
+
+  body.classList.add(
+    "sunset"
+  );
+
+}
+
+else{
+
+  body.classList.add(
+    "night"
+  );
+
+}
